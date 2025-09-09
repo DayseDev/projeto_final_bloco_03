dc84916a78a1f9501a2f2ac7a3ffc1847df0f1ef
unknown <dayse_santos22@hotmail.com>
2025-09-09 15:01:33 -0300

    Adiciona CRUD Categoria

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
+import ListaCategorias from './components/categoria/listacategorias/ListaCategorias'
+import FormCategoria from './components/categoria/formcategoria/FormCategoria'
 
 function App() {
   return (
@@ -11,8 +13,10 @@ function App() {
         <div className="min-h-[80vh]">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />}
-            />
+            <Route path="/home" element={<Home />} />
+            <Route path="/temas" element={<ListaCategorias />} />
+            <Route path="/cadastrartema" element={<FormCategoria />} />
+            <Route path="/editartema/:id" element={<FormCategoria />} />
           </Routes>
         </div>
         <Footer />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -10,11 +10,8 @@ function Navbar() {
                     <Link to='/home' className="text-2xl font-bold">Farmacia</Link>
 
                     <div className='flex gap-4'>
-                        Produtos
-                        Categorias
-                        Cadastrar categorias
-                        Perfil
-                        Sair
+                        <Link to='/temas' className='hover:underline'>Categorias</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar Categoria</Link>
                     </div>
                 </div>
             </div>
